dd3d4994e134e5549dccdb8e3b552273d9f82837
Hugo Caruso <hugoismyname@yahoo.com>
2020-09-09 08:27:06 -0400

    added login and logout based on satte

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -1,20 +1,42 @@
 import React from "react";
-import { Box, Link, Flex } from "@chakra-ui/core";
+import { Box, Link, Flex, Button } from "@chakra-ui/core";
 import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
 
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
-  return (
-    <Flex bg="tomato" p={4}>
-      <Box ml={"auto"}>
+  const [{ data, fetching }] = useMeQuery();
+
+  let navLinks = null;
+
+  //data is loading
+  if (fetching) {
+    navLinks = null;
+    //user not logged in
+  } else if (!data?.me) {
+    navLinks = (
+      <>
         <NextLink href="/login">
           <Link mr={2}>login</Link>
         </NextLink>
         <NextLink href="/register">
           <Link mr={2}>register</Link>
         </NextLink>
-      </Box>
+      </>
+    );
+    // user logged in
+  } else {
+    navLinks = (
+      <Flex>
+        <Box mr={2}>{data.me.username}</Box>
+        <Button variant="link">logout</Button>
+      </Flex>
+    );
+  }
+  return (
+    <Flex bg="tomato" p={4}>
+      <Box ml={"auto"}>{navLinks}</Box>
     </Flex>
   );
 };
